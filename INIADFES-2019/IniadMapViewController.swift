235b25083e237f4bfafac032dc2c8b5e657f3fef
Kentaro <kentaro@whim.jp>
2019-09-16 23:13:23 +0900

    3Fのマップ表示テスト・出展表示のUI定義

---
+++ INIADFES-2019/IniadMapViewController.swift
@@ -0,0 +1,25 @@
+//
+//  IniadMapViewController.swift
+//  INIADFES-2019
+//
+//  Created by Kentaro on 2019/09/16.
+//  Copyright © 2019 Kentaro. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Alamofire
+import SwiftyJSON
+import KeychainAccess
+
+class IniadMapViewController:UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+}
