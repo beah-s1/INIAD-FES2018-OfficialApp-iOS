8e2235f3c2f92bdefeeb37228a3f8422d3d3dfd2
Kentaro <kentaro@whim.jp>
2019-09-24 14:43:39 +0900

    設定関係

---
+++ INIADFES-2019/ConfirgurationController.swift
@@ -0,0 +1,29 @@
+//
+//  ConfirgurationController.swift
+//  INIADFES-2019
+//
+//  Created by Kentaro on 2019/09/24.
+//  Copyright © 2019 Kentaro. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Configuration{
+    private var dict:NSDictionary
+    
+    init() {
+        guard let path = Bundle.main.path(forResource: "configuration", ofType: "plist") else{
+            assert(false, "COULD NOT FIND CONFIGURATION FILE")
+        }
+        self.dict = NSDictionary(contentsOfFile: path)!
+    }
+    
+    func forKey(key:String) -> String{
+        if let value = self.dict[key] as? String{
+            return value
+        }else{
+            return ""
+        }
+    }
+}
